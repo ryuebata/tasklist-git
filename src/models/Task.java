a01da4cc14fc92faad6a8968136dd14f3c5d5da6
ryu.ebata <ryu.6991.pold@gmail.com>
2020-09-13 15:30:12 +0900

    Add NewServlet.java

--- src/models/Task.java
+++ src/models/Task.java
@@ -15,7 +15,8 @@ import javax.persistence.Table;
 @NamedQueries({
     @NamedQuery(
             name = "getAllTasks",
-            query = "SELECT m FROM Task AS m ORDER BY m.id DESC")
+            query = "SELECT m FROM Task AS m ORDER BY m.id DESC"
+            )
 })
 @Table(name = "tasks")
 public class Task {
@@ -24,14 +25,14 @@ public class Task {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
+    @Column(name = "content", length = 255, nullable = false)
+    private String content;
+
     @Column(name = "created_at", nullable = false)
     private Timestamp created_at;
 
     @Column(name = "updated_at", nullable = false)
     private Timestamp updated_at;
-
-    @Column(name = "content", length = 255, nullable = false)
-    private String content;
 
     //Getter Setter
     public Integer getId() {
@@ -40,6 +41,14 @@ public class Task {
 
     public void setId(Integer id) {
         this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
     }
 
     public Timestamp getCreated_at() {
@@ -58,11 +67,4 @@ public class Task {
         this.updated_at = updated_at;
     }
 
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
 }
